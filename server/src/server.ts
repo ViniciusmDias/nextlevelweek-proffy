7e1f07bcd5d89f36a4d02b978e44793187214fb7
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-08-10 15:53:55 -0300

    Create tables in database sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,11 @@ import express from 'express'
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/users', (request, response) => {
-  return response.json('Hello World')
+
+  return response.json({ message: 'Hello World' })
 })
 
 app.listen(3333)
